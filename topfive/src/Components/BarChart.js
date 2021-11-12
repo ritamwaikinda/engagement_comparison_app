e924064e73a0a4d81bcd680628efdc191e9ca8e9
Rita Mwaikinda <srmwaikinda@gmail.com>
2021-11-12 15:33:38 -0500

    bar chart datasets

--- topfive/src/Components/BarChart.js
+++ topfive/src/Components/BarChart.js
@@ -3,7 +3,41 @@ import { Bar } from "react-chartjs-2";
 
 const BarChart = () => {
     return (
-        <div>Bar Chart</div>
+        <div>
+            <Bar 
+                data={{
+                    labels: ['Brands', 'Or', 'Array', 'One', 'Two', 'Three'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [1, 2, 3, 4, 5, 6],
+                        backgroundColor: [
+                            red, blue, green, yellow, orange, purple
+                        ],
+                        borderColor: [
+                            black, brown, grey, beige, pink, white
+                        ],
+                        borderWidth: 1
+                    }],
+                }}
+                options={{
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }}
+                height={400}
+                width={600}
+                options={{
+                    maintainAspectRatio: false,
+                }}
+            />
+            
+        
+        
+        
+        
+        </div>
     )
 }
 
